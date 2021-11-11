bfbd57d30e726820fb62242e183b9a8de5e1859f
Tấn Nhạc <78245180+tannhac2310@users.noreply.github.com>
2021-11-12 00:16:52 +0700

    First Commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+
+const path = require("path");
+
+const cors = require("cors");
+
+const morgan = require("morgan");
+const { resourceLimits } = require('worker_threads');
+const route = require('./routes');
+
+
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
+
+route(app);
+
+// app.get("/", (req, res)=> {
+//     res.send("OK rồi!");
+// });
+
+
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port: ${port} `);
+});
---
+++ server/src/routes/index.js
@@ -0,0 +1,6 @@
+const siteRouter = require("./site");
+function route(app) {
+    app.use("");
+}
+
+module.exports = route;
